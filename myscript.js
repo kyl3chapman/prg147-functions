1da1440b1e11902017a7b8e37992525c3b5865df
kyl3chapman <kyl3chapman@gmail.com>
2016-09-12 17:36:43 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,41 @@
+
+function change01(){
+ document.getElementById("large").src="datatypes.PNG";
+}
+
+function change02(){
+    document.getElementById("large").src="printtoscreen.PNG";
+}
+
+function change03(){
+    document.getElementById("large").src="monthly.PNG";
+}
+
+function change04(){
+    document.getElementById("large").src="nemo.PNG";
+}
+
+function change05(){
+    document.getElementById("large").src="bird.jpg";
+}
+
+function change06(){
+    document.getElementById("large").src="jester.jpg";
+}
+
+function change07(){
+    document.getElementById("large").src="tree.jpg";
+}
+
+function change08(){
+    document.getElementById("large").src="dome.jpg";
+}
+
+function change09(){
+    document.getElementById("large").src="spartan.jpg";
+}
+
+function change10(){
+    document.getElementById("large").src="flower.jpg";
+}
+
